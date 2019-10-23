60602ff082cc0ef7528ded4f511439198af446b1
JeanMax-29 <181727@student.umons.ac.be>
2019-10-23 15:23:28 +0200

    YESSS

--- src/main/java/exercise2/Exercise2.java
+++ src/main/java/exercise2/Exercise2.java
@@ -2,28 +2,21 @@ package exercise2;
 
 public class Exercise2 {
 
-	// Ajoutez un champ age à chaque instance de la classe Person.
-	// Créez-y une méthode statique "computePopulationSize" pour retourner la taille de la population.
-	// Créez-y une méthode statique "computeAveragePopulationAge" pour calculer l'âge moyen de la population.
-	// Implémentez-y une méthode statique "resetPopulation" pour remettre les compteurs à zéro.
-
 	static void createPerson(int age) {
 	}
 
 	static int computePopulationSize() {
 		// TODO remove comment when implemented
-		// return Person.computePopulationSize();
-		return 0;
+		return Person.computePopulationSize();
 	}
 
 	static float computeAveragePopulationAge() {
 		// TODO remove comment when implemented
-		// return Person.computeAveragePopulationAge();
-		return 0.0F;
+		return Person.computeAveragePopulationAge();
 	}
 
 	static void resetPopulation() {
 		// TODO remove comment when implemented
-		// Person.resetPopulation();
+		Person.resetPopulation();
 	}
 }
--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,26 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int totalAge = 0;
+	private static int populationSize = 0;
+
+	public Person(int age){
+		this.age = age;
+		totalAge += this.age;
+		populationSize++;
+	}
+
+	public static int computePopulationSize(){
+		return populationSize;
+	}
+
+	public static float computeAveragePopulationAge(){
+		return totalAge/populationSize;
+	}
+
+	public static void resetPopulation(){
+		totalAge=0;
+		populationSize=0;
+	}
 }
