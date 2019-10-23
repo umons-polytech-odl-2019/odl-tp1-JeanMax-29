ea28bb2be5864f004e4c0d9eb2c0b9f4ddcd30c9
JeanMax-29 <181727@student.umons.ac.be>
2019-10-23 15:10:19 +0200

    YESSS

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,23 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	Person(String n,int a){
+		name = n;
+		age = a;
+	}
+	public void setName(String n){
+		name=n;
+	}
+	public void setAge(int a){
+		age=a;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getAge(){
+		return age;
+	}
 }
